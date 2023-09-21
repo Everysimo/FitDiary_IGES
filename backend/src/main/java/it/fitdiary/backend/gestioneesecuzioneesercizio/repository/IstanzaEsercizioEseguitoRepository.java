d5b5c4376ca2aca95d38c199bb1176a8d2ec240a
Pio57 <pio57_2015@libero.it>
2023-09-21 15:21:36 +0200

    feat: Implementation GestioneIstanzaEsercizioEseguito

---
+++ backend/src/main/java/it/fitdiary/backend/gestioneesecuzioneesercizio/repository/IstanzaEsercizioEseguitoRepository.java
@@ -0,0 +1,14 @@
+package it.fitdiary.backend.gestioneesecuzioneesercizio.repository;
+
+import it.fitdiary.backend.entity.IstanzaEsercizio;
+import it.fitdiary.backend.entity.IstanzaEsercizioEseguito;
+import it.fitdiary.backend.entity.Protocollo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public interface IstanzaEsercizioEseguitoRepository extends JpaRepository<IstanzaEsercizioEseguito, Long> {
+
+    List<IstanzaEsercizioEseguito> findAllByProtocolloAndIstanzaEsercizio(@NotNull(message = "Il protocollo non può essere nullo") Long idProtocollo, @NotNull(message = "L'istanza dell'esercizio non può essere nullo") Long idIstanzaEsercizio);
+}
